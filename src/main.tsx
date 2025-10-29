75319a3de073cddaee83b64978b691e0fd0785a6
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-10-29 21:21:38 +0300

    app ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import { App } from "./App.tsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
